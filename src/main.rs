111cb8c742d15135c96ee027501ca967440fc021
Arthur <arthrp@users.noreply.github.com>
2020-08-26 21:52:45 +0300

    Allowing to specify custom port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::str::FromStr;
 
 #[tokio::main]
 async fn main() {
@@ -6,11 +7,17 @@ async fn main() {
     let args : Vec<String> = env::args().collect();
     let path = args[1].clone();
 
-    let port = 3030;
+    let port: u16 = if (args.len() > 2) { get_port(&args[2]) } else { 3030 };
 
     println!("Serving {} on {}", path, port);
 
     warp::serve(warp::fs::dir(path))
         .run(([127, 0, 0, 1], port))
         .await;
+}
+
+fn get_port(port_str: &str) -> u16 {
+    let port = u16::from_str(port_str).unwrap();
+
+    return port;
 }
