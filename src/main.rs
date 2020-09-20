9cf09cb855373f8d2bf9bc5660b57aaef6205609
Arthur <arthrp@users.noreply.github.com>
2020-09-20 03:07:25 +0300

    Printing usage and checking that folder exists

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+#![allow(unused_parens)]
 use std::env;
 use std::str::FromStr;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() {
 
     let args : Vec<String> = env::args().collect();
+    if(args.len() < 2){
+        print_usage();
+        return;
+    }
+
     let path = args[1].clone();
+
+    if(!Path::new(&path).exists()) {
+        println!("Path {} doesn't exist", path);
+        return;
+    }
 
     let port: u16 = if (args.len() > 2) { get_port(&args[2]) } else { 3030 };
 
@@ -20,4 +32,8 @@ fn get_port(port_str: &str) -> u16 {
     let port = u16::from_str(port_str).unwrap();
 
     return port;
+}
+
+fn print_usage() -> () {
+    println!("Usage: localserver <path> <port>");
 }
